de25fed2b5e3564cbf09d96efb295f737162da88
RawrBear <rawrbearmedia@gmail.com>
2022-06-11 13:46:23 -0400

    Added a connection to the database

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const { MongoClient } = require("mongodb");
 const express = require("express");
+let db;
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+    const allAnimals = await db.collection("animals").find().toArray();
+    console.log("allAnimals", allAnimals);
     res.send("Welcome to the home page");
 });
 
@@ -10,4 +14,13 @@ app.get("/admin", (req, res) => {
     res.send("Welcome to the admin page");
 });
 
-app.listen(3000);
+async function start() {
+    const client = new MongoClient(
+        "mongodb://root:root@localhost:27017/MernPetApp?authSource=admin"
+    );
+    await client.connect();
+    db = client.db();
+    app.listen(3000);
+}
+
+start();
