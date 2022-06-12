e071ecdafb6da3c84556ac51e169fbd833e12897
RawrBear <rawrbearmedia@gmail.com>
2022-06-11 20:18:54 -0400

    Hooked the form up to the database

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const express = require("express");
+const multer = require("multer");
+const upload = multer();
+const sanitizeHTML = require("sanitize-html");
 let db;
 
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 function passwordProtected(req, res, next) {
     res.set("WWW-Authenticate", 'Basic realm="Mern Pet App"');
@@ -33,6 +39,39 @@ app.get("/api/animals", async (req, res) => {
     const allAnimals = await db.collection("animals").find().toArray();
     res.json(allAnimals);
 });
+
+app.post(
+    "/create-animal",
+    upload.single("photo"),
+    cleanUp,
+    async (req, res) => {
+        console.log("Data", req.body);
+        const info = await db.collection("animals").insertOne(req.cleanData);
+        const newAnimal = await db
+            .collection("animals")
+            .findOne({ _id: new ObjectId(info.insertedId) });
+        res.send(newAnimal);
+    }
+);
+
+function cleanUp(req, res, next) {
+    if (typeof req.body.name !== "string") req.body.name = "";
+    if (typeof req.body.species !== "string") req.body.species = "";
+    if (typeof req.body._id !== "string") req.body._id = "";
+
+    req.cleanData = {
+        name: sanitizeHTML(req.body.name.trim(), {
+            allowedTags: [],
+            allowedAttributes: [],
+        }),
+        species: sanitizeHTML(req.body.species.trim(), {
+            allowedTags: [],
+            allowedAttributes: [],
+        }),
+    };
+
+    next();
+}
 
 async function start() {
     const client = new MongoClient(
